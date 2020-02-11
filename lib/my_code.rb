7d76aedf4ccebad1c31d703752c9f4963cbff2d9
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-11 19:40:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def reduce(source_array, starting_point=0)
   total = source_array[0]
   i = 1
  end
- while i < source_array.length do
+ while i < source_array.length 
   total = yield(total, source_array[i])
   i +=1
  end
