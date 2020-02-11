81d50ce420d65c4ca8626ac621355c6eeb04d50f
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-11 19:39:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,17 @@ end
 result
 end
 
+def reduce(source_array, starting_point=0)
+ if starting_point
+  total = starting_point
+  i = 0
+ else 
+  total = source_array[0]
+  i = 1
+ end
+ while i < source_array.length do
+  total = yield(total, source_array[i])
+  i +=1
+ end
+ total
+end
